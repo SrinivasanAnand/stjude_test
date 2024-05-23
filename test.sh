f3dd8b53e9db9803e445fca9446b9373bce22cdb
Srinivasan <asrini63@splprhpc08.cm.cluster>
2024-05-23 13:23:14 -0500

    added test script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#BSUB -P testVGG16
+#BSUB -q gpu
+#BSUB -n 2
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -gpu "num=2/host"
+
+module load pytorch/2.2.1
+pip install torchvision
+python3 vgg16test.py &> out_testing.txt
